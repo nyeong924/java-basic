d94699d9a9c052f699f29e072413b9a13ae63e16
nyeong924 <hjn42515@gmail.com>
2024-09-09 11:22:32 +0900

    git init

---
+++ src/final1/FieldInit.java
@@ -0,0 +1,9 @@
+package final1;
+
+public class FieldInit {
+
+    static final int CONSET_VALUE = 10;
+    final int value = 10;
+
+
+}
---
+++ src/oop1/ex/Account.java
@@ -0,0 +1,18 @@
+package oop1.ex;
+
+public class Account {
+    int balance = 0;
+
+    void deposit(int amount) {
+        balance += amount;
+    }
+
+    void withdraw(int amount) {
+        if(amount > balance){
+            System.out.println("잔액 부족");
+        } else {
+            balance -= amount;
+        }
+    }
+
+}
